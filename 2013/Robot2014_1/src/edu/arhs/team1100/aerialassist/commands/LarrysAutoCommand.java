e6b93f69412f722e552a1459ae706aa31d6d8acc
Larry@tambascio.net <Larry@tambascio.net@0d174aa7-39b9-d44f-4816-cc6ed2577b69>
2014-01-17 23:41:08 +0000

    Larry's auto command class

---
+++ 2013/Robot2014_1/src/edu/arhs/team1100/aerialassist/commands/LarrysAutoCommand.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.arhs.team1100.aerialassist.commands;
+
+import edu.arhs.team1100.aerialassist.commands.drive.DriveInTankCommand;
+import edu.arhs.team1100.aerialassist.commands.drive.StopDriveCommand;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ * Stupid command sample code to make robot do something.
+ *
+ * @author  Larry Tambascio
+ */
+public class LarrysAutoCommand extends CommandGroup
+{
+    
+    /**
+     * Constructor that initializes the steps.
+     */
+    public LarrysAutoCommand()
+    {
+        addSequential(new DriveInTankCommand(0.5, 0.5, 2));
+        addSequential(new DriveInTankCommand(0.25, -0.25, 2));
+        addSequential(new DriveInTankCommand(-0.5, -0.5, 2));
+        addSequential(new StopDriveCommand(0));
+    }
+}
