f27c5d8b4f2f44b4b764f7bd42fbfe585ea1b468
SupremeSteak1@gmail.com <SupremeSteak1@gmail.com@0d174aa7-39b9-d44f-4816-cc6ed2577b69>
2014-01-18 16:41:27 +0000

    Added The Team Template Class

---
+++ Scouting Database Application/src/scoutingdatabase/Team.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scoutingdatabase;
+
+/**
+ *
+ * @author Team1100
+ */
+public class Team {
+    int teamNumber;
+    int wins;
+    int losses;
+    boolean overTruss;
+    boolean canCatch;
+    String comments;
+    public Team() {
+        
+    }
+}
