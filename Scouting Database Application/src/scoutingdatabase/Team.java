85441ea9ca594be73b1399624d1ae126a766e30c
zacharysamiyoussef@gmail.com <zacharysamiyoussef@gmail.com@0d174aa7-39b9-d44f-4816-cc6ed2577b69>
2014-01-18 19:04:37 +0000

    Began working on adding teams and matches

--- Scouting Database Application/src/scoutingdatabase/Team.java
+++ Scouting Database Application/src/scoutingdatabase/Team.java
@@ -4,18 +4,46 @@
  */
 package scoutingdatabase;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Team1100
  */
 public class Team {
-    int teamNumber;
-    int wins;
-    int losses;
-    boolean overTruss;
-    boolean canCatch;
-    String comments;
-    public Team() {
+
+    int teamNumber; //Team Number
+    int matchNumber;
+    
+    int wins; //How many times the team wins
+    int losses; //How  many times the team looses
+    boolean passTruss;//Can pass a ball over the truss
+    //boolean catchTruss; //Can catch a ball thrown over a truss
+    boolean canCatch; //Can catch a ball tossed by our robot
+    boolean canPass; //Will be able to pass to our robot
+    String comments; //Additional Comments
+    int startingPosition; //Where the robot starts the match during autonomous
+    int score; //How much the team/alliance scores
+    boolean scoresAuto; //Fires in the goal during autonomous
+    boolean scoresHotGoal; //Fires hot goal during autonomous
+    boolean driveScoreAuto; //Drives in autonomous to score points
+    
+    
+    final int goaliePosition = 0;
+    final int leftPosition = 1;
+    final int rightPosition = 3;
+    final int middlePosition = 2;
+
+    private ArrayList<Integer> match = new ArrayList<Integer>();
+    
+    public Team(int teamNumber) {
         
+    }
+    private void match(){
+        match.add(matchNumber);
+    }
+   
+    public int getTeamNumber() {
+        return teamNumber;
     }
 }
--- Scouting Database Application/src/scoutingdatabase/main.java
+++ Scouting Database Application/src/scoutingdatabase/main.java
@@ -6,17 +6,35 @@
 
 package scoutingdatabase;
 
+import java.util.ArrayList;
+
+
 /**
  *
- * @author Owner
+ * @author Team 1100
  */
 public class main {
+    ArrayList<Team> teams = new ArrayList<Team>();
 
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
     }
+    
+    public void addTeam(int teamNumber) {
+        for(int i = 0; i < teams.size(); i++) {
+            //Test to see if the team already exsists
+            if(teams.get(i).getTeamNumber() == teamNumber) {
+                break;
+                
+            }
+        }
+        //Adds the new team.
+        teams.add(new Team(teamNumber));
+    } 
+ 
+    
     
 }
