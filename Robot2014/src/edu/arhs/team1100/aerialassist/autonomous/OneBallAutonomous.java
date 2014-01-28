94a5d55bf997e6bbbc0044740ef891c90ed93e3f
zacharysamiyoussef@gmail.com <zacharysamiyoussef@gmail.com@0d174aa7-39b9-d44f-4816-cc6ed2577b69>
2014-01-28 00:39:28 +0000

    Added some psuedo  code for one ball in auto

---
+++ Robot2014/src/edu/arhs/team1100/aerialassist/autonomous/OneBallAutonomous.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.arhs.team1100.aerialassist.autonomous;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import edu.arhs.team1100.aerialassist.commands.drive.DriveInALineCommand;
+import edu.arhs.team1100.aerialassist.commands.drive.DriveInTankCommand;
+import edu.arhs.team1100.aerialassist.subsystems.DriveSubsystem;
+import edu.arhs.team1100.aerialassist.commands.drive.StopDriveCommand;
+import edu.arhs.team1100.aerialassist.commands.manipulatorcommands.FireShooterCommand;
+
+/**
+ *
+ * @author Team 1100
+ */
+public class OneBallAutonomous extends CommandGroup {
+
+    /**
+     * Fires one ball. WIll test first to see if drive mode is mehcanum or tank,
+     * then will test/wait for hot goal, then fire.
+     *
+     * @param speed = speed of wheels from -1 to 1
+     * @param duration = duration of movement
+     */
+    public void OneBallCommand(double speed, double duration) {
+
+        //Tests drive mode, then moves forward
+        if (DriveSubsystem.getInstance().getDriveMode() == DriveSubsystem.MODE_TANK) {
+            addSequential(new DriveInTankCommand(speed, speed, duration));
+        }//else: run same thing for mehcanum
+
+        //Put camera code here after, instead of true ;)
+        boolean replaceWithCameraHotGoalTest = true;
+        if (replaceWithCameraHotGoalTest) {
+            addSequential(new FireShooterCommand());
+        } else {
+            addSequential(new WaitCommand(5000));
+            addSequential(new FireShooterCommand());
+        }
+    }
+}
