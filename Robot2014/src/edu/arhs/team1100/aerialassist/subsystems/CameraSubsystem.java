ae2962d0ed9df44589cff00bcd21fb78d2b11b44
zacharysamiyoussef@gmail.com <zacharysamiyoussef@gmail.com@0d174aa7-39b9-d44f-4816-cc6ed2577b69>
2014-01-31 23:00:22 +0000

    Added some psuedo  code for the camera subsystem

---
+++ Robot2014/src/edu/arhs/team1100/aerialassist/subsystems/CameraSubsystem.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.arhs.team1100.aerialassist.subsystems;
+import edu.arhs.team1100.aerialassist.OI;
+import edu.arhs.team1100.aerialassist.RobotMap;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+
+/**
+ *
+ * @author Team 1100
+ */
+public class CameraSubsystem extends Subsystem {
+    
+    static CameraSubsystem instance;
+    private Talon armMotor;
+    private Talon wheelMotorA;
+    private Talon wheelMotorB;
+    private double wheelSpeed = .5;
+    /**
+     * Constructs an ISubsystem. Initializes compressor, lift pistons,
+     * intake motors. Starts compressor.
+     */
+    public CameraSubsystem() {
+        armMotor = new Talon(RobotMap.M_ARM);
+        wheelMotorA = new Talon(RobotMap.M_RIGHT_WHEEL);
+        wheelMotorB = new Talon(RobotMap.M_LEFT_WHEEL);
+    }
+
+    /**
+     * Creates a IntakeSubsystem if not already created
+     *
+     * @return instance
+     */
+    public static CameraSubsystem getInstance() {
+        if (instance == null) {
+            instance = new CameraSubsystem();
+            instance.initDefaultCommand();
+        }
+        return instance;
+    }
+
+
+    public void seeHotGoal()
+    {
+        
+    }
+    
+    public void seeReflectiveTape(){
+        
+    }
+    
+    
+    /**
+     * Initializes shooter command. Do nothing.
+     */
+    protected void initDefaultCommand() {
+        seeHotGoal();
+    }
+}
